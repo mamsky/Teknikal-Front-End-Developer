bef0f0a13eaa7d42b9814510c3b23dcc0465da6f
Paste Prosmana <pasteprosmana189@gmail.com>
2025-07-26 15:01:27 +0700

    Feat: list product

--- src/feature/ListProduct.tsx
+++ src/feature/ListProduct.tsx
@@ -10,11 +10,16 @@ const ListProduct = ({ data }: { data: ProductDTO }) => {
       <div className="text-end">
         <DropdownOption data={data} />
       </div>
-      <img
-        src={data.images}
-        alt={data.nama}
-        className="rounded-md h-46 max-h-46 bg-cover min-w-full bg-center"
-      />
+      <div className="relative">
+        <h1 className="absolute left-2 bg-red-500 p-2 top-2 rounded-md font-bold text-white">
+          Diskon Harga
+        </h1>
+        <img
+          src={data.images}
+          alt={data.nama}
+          className="rounded-md h-46 max-h-46 bg-cover min-w-full bg-center"
+        />
+      </div>
       <h1 className="font-bold">{data.nama} </h1>
       <small>Stock: {data.stock}</small>
       <p>
